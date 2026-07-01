4e4b5ce57dd97de96c9afd58de4afc0452262dae
notihait <gr.maria09@gmail.com>
2026-07-01 17:38:40 +0300

    got id! now ui updates

--- bot/bot.rb
+++ bot/bot.rb
@@ -9,6 +9,34 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |msg|
     next unless msg.text == "/start"
 
+    user = msg.from
+
+    nick =
+      if user.username
+        "@#{user.username}"
+      else
+        user.first_name
+      end
+
+    text = <<~TEXT
+    👋 Привет, #{nick} ^_^!
+
+    🎉 Я бот для управления вишлистами!
+
+    ✨ Что я умею:
+    • Напоминать о предстоящих событиях
+    • Показывать вишлисты по ссылке
+    • Помогать не забыть о важных датах
+
+    💡 Совет: Все вишлисты создаются и управляются через удобную миниапп!
+    Там вы можете:
+    • Создавать вишлисты с датами
+    • Добавлять подарки с фото и ссылками
+    • Делиться вишлистами с друзьями
+
+    🚀 Откройте миниапп и начните создавать свои вишлисты!
+    TEXT
+
     keyboard = Telegram::Bot::Types::InlineKeyboardMarkup.new(
       inline_keyboard: [
         [
@@ -22,7 +50,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
 
     bot.api.send_message(
       chat_id: msg.chat.id,
-      text: "Open Mini App",
+      text: text,
       reply_markup: keyboard
     )
   end
