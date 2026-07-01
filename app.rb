a12808e2e8b5c151229bd31753ab30ef15e81729
notihait <gr.maria09@gmail.com>
2026-07-01 19:40:53 +0300

    adding creating9233754654555к45

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
 require "sinatra"
+require "sinatra/base"
+set :host_authorization, false
 
 require_relative "./app/controllers/api/auth_controller"
 require_relative "./app/controllers/wishlists_controller"
 
 use AuthController
 use WishlistsController
-
-
-set :host_authorization, permitted_hosts: ["iwishlist.ru", "www.iwishlist.ru"]
 
 set :bind, "0.0.0.0"
 set :port, 4567
