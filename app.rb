21153b07758e29c8503ab791c6220a9790ccb910
notihait <gr.maria09@gmail.com>
2026-07-01 19:43:56 +0300

    adding creating9233754654555к45342

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require "sinatra"
 require "sinatra/base"
-set :host_authorization, false
 
 require_relative "./app/controllers/api/auth_controller"
 require_relative "./app/controllers/wishlists_controller"
@@ -12,4 +11,5 @@ set :bind, "0.0.0.0"
 set :port, 4567
 set :public_folder, File.join(__dir__, "public")
 
-set :protection, except: :host_authorization
+# 🔥 просто отключаем host protection правильно
+set :host_authorization, permitted_hosts: ["iwishlist.ru", "www.iwishlist.ru"]
