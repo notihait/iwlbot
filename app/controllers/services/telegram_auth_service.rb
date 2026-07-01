2026554bc2729da640bfcbfaa95c8f62dbf132cc
notihait <gr.maria09@gmail.com>
2026-07-01 18:20:46 +0300

    adding creating

--- app/controllers/services/telegram_auth_service.rb
+++ app/controllers/services/telegram_auth_service.rb
@@ -1,16 +1,26 @@
-# app/services/telegram_auth_service.rb
 require "openssl"
-require "uri"
+require "json"
 require "cgi"
+require_relative "../db/connection"
 
 class TelegramAuthService
   def self.call(init_data)
-    # тут будет проверка подписи Telegram
-    # (очень важно для безопасности Mini App)
-
     parsed = CGI.parse(init_data)
+    user_json = parsed["user"]&.first
+    user_data = JSON.parse(user_json)
 
-    user = parsed["user"]&.first
-    JSON.parse(user)["id"]
+    telegram_id = user_data["id"]
+    first_name = user_data["first_name"]
+    username = user_data["username"]
+
+    result = DB.conn.exec_params(<<~SQL, [telegram_id, first_name, username])
+      INSERT INTO users (telegram_id, first_name, username)
+      VALUES ($1, $2, $3)
+      ON CONFLICT (telegram_id)
+      DO UPDATE SET first_name = EXCLUDED.first_name
+      RETURNING id
+    SQL
+
+    result[0]["id"] # <-- ВАЖНО: это users.id
   end
 end
