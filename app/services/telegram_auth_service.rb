82f65dcabaad5f3561d1275e32042a9ad41c7c42
notihait <gr.maria09@gmail.com>
2026-07-01 19:05:16 +0300

    adding creating9

--- app/services/telegram_auth_service.rb
+++ app/services/telegram_auth_service.rb
@@ -1,12 +1,16 @@
-require "openssl"
+require "uri"
 require "json"
-require "cgi"
 require_relative "../db/connection"
 
 class TelegramAuthService
   def self.call(init_data)
-    parsed = CGI.parse(init_data)
-    user_json = parsed["user"]&.first
+    # Парсим Telegram initData (правильный способ)
+    params = URI.decode_www_form(init_data).to_h
+
+    user_json = params["user"]
+
+    raise "user missing in initData" if user_json.nil? || user_json.strip.empty?
+
     user_data = JSON.parse(user_json)
 
     telegram_id = user_data["id"]
@@ -17,10 +21,11 @@ class TelegramAuthService
       INSERT INTO users (telegram_id, first_name, username)
       VALUES ($1, $2, $3)
       ON CONFLICT (telegram_id)
-      DO UPDATE SET first_name = EXCLUDED.first_name
+      DO UPDATE SET first_name = EXCLUDED.first_name,
+                    username = EXCLUDED.username
       RETURNING id
     SQL
 
-    result[0]["id"] # <-- ВАЖНО: это users.id
+    result[0]["id"]
   end
 end
