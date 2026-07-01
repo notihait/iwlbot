4db72074a9ddc6de9f73bd52e64bc00d4c4d5cbc
notihait <gr.maria09@gmail.com>
2026-07-01 17:31:04 +0300

    tryin to get tg id12334533122

--- app.rb
+++ app.rb
@@ -2,11 +2,8 @@ require "sinatra"
 
 set :bind, "0.0.0.0"
 set :port, 4567
+set :public_folder, File.join(__dir__, "public")
 
 before do
-  content_type :text
-end
-
-get "/" do
-  "IWLBOT OK"
+  content_type :html
 end
