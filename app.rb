25b5caa42ca6926ba6706d88ae291d962ddb9ac6
notihait <gr.maria09@gmail.com>
2026-07-02 14:15:40 +0300

    fixes1232363

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ require "dotenv/load"
 require_relative "./app/controllers/api/auth_controller"
 require_relative "./app/controllers/wishlists_controller"
 
+set :protection, except: :host_authorization
+
 use AuthController
 use WishlistsController
 
 set :bind, "0.0.0.0"
 set :port, 4567
 set :public_folder, File.join(__dir__, "public")
-
-set :protection, except: :host_authorization
