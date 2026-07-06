d13941b2025337a9a3059dc2233499136a269bc5
notihait <gr.maria09@gmail.com>
2026-07-06 21:31:00 +0300

    notify123456

--- app/services/notify_followers_service.rb
+++ app/services/notify_followers_service.rb
@@ -8,7 +8,7 @@ class NotifyFollowersService
     bot_token = ENV["BOT_TOKEN"]
     return if bot_token.to_s.empty?
 
-    full_text = "#{text}\n\n#{wishlist_link(wishlist)}"
+    full_text = "#{text} #{wishlist_link_html(wishlist)}"
 
     wishlist.followers.each do |follower|
       next if follower.telegram_id.nil?
@@ -28,13 +28,19 @@ class NotifyFollowersService
     send_message(bot_token, owner.telegram_id, text)
   end
 
-  def self.wishlist_link(wishlist)
-    "https://t.me/#{BOT_USERNAME}?startapp=wishlist_#{wishlist.public_id}"
+  def self.wishlist_link_html(wishlist)
+    url = "https://t.me/#{BOT_USERNAME}?startapp=wishlist_#{wishlist.public_id}"
+    title = escape_html(wishlist.title)
+    "<a href=\"#{url}\">#{title}</a>"
+  end
+
+  def self.escape_html(str)
+    str.to_s.gsub("&", "&amp;").gsub("<", "&lt;").gsub(">", "&gt;")
   end
 
   def self.send_message(bot_token, chat_id, text)
     uri = URI.parse("https://api.telegram.org/bot#{bot_token}/sendMessage")
-    Net::HTTP.post_form(uri, "chat_id" => chat_id, "text" => text)
+    Net::HTTP.post_form(uri, "chat_id" => chat_id, "text" => text, "parse_mode" => "HTML")
   rescue => e
     warn "Failed to notify chat_id=#{chat_id}: #{e.class} - #{e.message}"
   end
